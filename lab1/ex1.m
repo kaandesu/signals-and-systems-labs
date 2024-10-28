1924decb3cf1ebc7c79410f457002cd6a93cf5df
kaan <kaandesu00@gmail.com>
2024-10-28 23:03:49 +0100

    chore: updated the ex1

--- lab1/ex1.m
+++ lab1/ex1.m
@@ -3,17 +3,27 @@ close all;
 clear;
 format compact;
 
-i = 20;
-n = -i:1:i;
+% i manually tried
+i = 83333; 
+n = -i:i;
 N = length(n);
 
-% Case a: |a| < 1
-a1 = 0.5; 
-x1 = a1.^abs(n); 
-% Energy of x1
+a1 = 0.5;
+x1 = a1.^abs(n);
+
 energy_x1 = sum(abs(x1).^2);
-% Average power of x1
-avg_power_x1 = energy_x1 / N
+avg_power_x1 = energy_x1 / N;
+
+target_error = 1e-5; 
+
+fprintf('Extension size i: %d\n', i);
+fprintf('average power: %.10f\n', avg_power_x1);
+
+if avg_power_x1 < target_error
+    fprintf('Average power meets the 0.001%% tolerance.\n');
+else
+    fprintf('Average power does NOT meet the 0.001%% tolerance.\n');
+end
 
 figure;
 stem(n, x1, 'filled', 'LineWidth', 1.5);
@@ -22,7 +32,6 @@ xlabel('n');
 ylabel('x[n]');
 grid on;
 
-% Case 1: |a| > 1
 a2 = 1.2; 
 x2 = a2.^abs(n);
 
