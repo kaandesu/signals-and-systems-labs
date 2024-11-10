c8a5c96074ee2337125cecdb8699b5fab993ddd7
kaan <kaandesu00@gmail.com>
2024-11-10 15:45:31 +0100

    chore: plotted the Walsh basis

--- lab1/ex4.m
+++ lab1/ex4.m
@@ -23,7 +23,7 @@ xlabel('Index');
 ylabel('Magnitude');
 grid on;
 
-% Fourier Basis
+% Creating the Fourier Basis
 n = 0:N-1;
 UF = zeros(N);
 for k = 0:N-1
@@ -38,13 +38,22 @@ UF = (1/sqrt(N)) * UF;
 UB = eye(N);
 
 c_Fourier = UF' * x';
+
+U_W = (1/sqrt(N)) * hadamard(N);
+
+c_Walsh = U_W' * x';
+
 testMatrix = UF * conj(UF');
 testMatrix2 = UB * conj(UB');
+testMatrix3 = U_W * conj(U_W');
 
 sum(testMatrix(:)) % good if ~128
 sum(testMatrix2(:)) % good if ~128
+sum(testMatrix3(:)) % good if ~128
 
+% Plot Fourier Basis
 % Real part
+figure(2)
 subplot(2, 1, 1); % 2 rows, 1 column, first subplot
 stem(0:N-1, real(c_Fourier), 'filled', 'LineWidth', 1.5);
 title('Real Part of Projection Coefficients on Fourier Basis');
@@ -56,6 +65,14 @@ grid on;
 subplot(2, 1, 2); % 2 rows, 1 column, second subplot
 stem(0:N-1, imag(c_Fourier), 'filled', 'LineWidth', 1.5);
 title('Imaginary Part of Projection Coefficients on Fourier Basis');
+xlabel('Index');
+ylabel('Magnitude');
+grid on;
+
+% Plot Walsh Basis
+figure
+stem(0:N-1, real(c_Walsh),'filled','LineWidth',1.5);
+title('Projection Coefficients on Walsh Basis');
 xlabel('Index');
 ylabel('Magnitude');
 grid on;
